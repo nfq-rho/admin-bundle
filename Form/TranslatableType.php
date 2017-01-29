9417fe545961a971120f072a03bbcbd8a000c866
Paulius Kaminskas <paulius.kaminskas@nfq.lt>
2017-01-29 19:25:39 +0200

    Removed unused use

--- Form/TranslatableType.php
+++ Form/TranslatableType.php
@@ -15,7 +15,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * Class TranslatableType
