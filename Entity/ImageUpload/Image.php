cb2d3faeb2ff945ff4c1bf5a6c4a1f30276207d2
Paulius Kaminskas <paulius.kaminskas@nfq.lt>
2018-11-16 16:32:54 +0200

    Base embeddable entity for image upload support in entities

---
+++ Entity/ImageUpload/Image.php
@@ -0,0 +1,140 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the "NFQ Bundles" package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nfq\AdminBundle\Entity\ImageUpload;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Embeddable()
+ */
+class Image
+{
+    /**
+     * @var string
+     *
+     * @Gedmo\Translatable(fallback=true)
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Translatable(fallback=true)
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $altText;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Translatable(fallback=true)
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $copyright;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Translatable(fallback=true)
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $mimeType;
+
+    /**
+     * @var int
+     * @Gedmo\Translatable(fallback=true)
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    protected $size;
+
+    /**
+     * @var int[]
+     *
+     * @Gedmo\Translatable(fallback=true)
+     * @ORM\Column(type="simple_array", nullable=true)
+     */
+    protected $dimensions;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAltText(): ?string
+    {
+        return $this->altText;
+    }
+
+    public function setAltText(?string $altText): self
+    {
+        $this->altText = $altText;
+
+        return $this;
+    }
+
+    public function getCopyright(): ?string
+    {
+        return $this->copyright;
+    }
+
+    public function setCopyright(?string $copyright): self
+    {
+        $this->copyright = $copyright;
+
+        return $this;
+    }
+
+    public function getMimeType(): ?string
+    {
+        return $this->mimeType;
+    }
+
+    public function setMimeType(?string $mimeType): self
+    {
+        $this->mimeType = $mimeType;
+
+        return $this;
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function setSize(?int $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getDimensions(): ?array
+    {
+        return $this->dimensions;
+    }
+
+    public function setDimensions(?array $dimensions): self
+    {
+        $this->dimensions = $dimensions;
+
+        return $this;
+    }
+}
